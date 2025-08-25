b7602aa83325987d8095acd2421a074141d2cf4d
sunwoomk <ahnga3@gmail.com>
2025-08-25 17:48:20 +0900

    에셋추가, 멀티 씬 테스트 중

---
+++ Assets/Scripts/BattleSceneManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BattleSceneManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SceneManager.LoadScene("BattleCharacterScene", LoadSceneMode.Additive);
+        SceneManager.LoadScene("MiniGameScene", LoadSceneMode.Additive);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
