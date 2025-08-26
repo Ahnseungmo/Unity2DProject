b9956aaaf9efb5cceed432ec6c230ab650afd22d
sunwoomk <ahnga3@gmail.com>
2025-08-26 17:48:30 +0900

    멀티씬 테스트중

--- Assets/Scripts/BattleSceneManager.cs
+++ Assets/Scripts/BattleSceneManager.cs
@@ -6,8 +6,10 @@ public class BattleSceneManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        /*
         SceneManager.LoadScene("BattleCharacterScene", LoadSceneMode.Additive);
         SceneManager.LoadScene("MiniGameScene", LoadSceneMode.Additive);
+        */
     }
 
     // Update is called once per frame
