00fdce99094524651be942a3db451b190ea058cb
Ahnseungmo <ahnga3@gmail.com>
2025-08-27 12:04:25 +0900

    오브젝트 슈팅 작성중

---
+++ Assets/Scripts/MiniGame/Projectile.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 dragStartPos;
+    private bool isDragging = false;
+
+    [SerializeField] private float power = 5f;   
+    [SerializeField] private float maxDrag = 3f; 
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnMouseDown()
+    {
+        dragStartPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        isDragging = true;
+        rb.linearVelocity = Vector2.zero;
+        rb.bodyType = RigidbodyType2D.Kinematic; 
+    }
+
+    void OnMouseDrag()
+    {
+        if (!isDragging) return;
+
+        Vector2 currentPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 dragVector = currentPos - dragStartPos;
+
+        dragVector = Vector2.ClampMagnitude(dragVector, maxDrag);
+
+        Vector2 newPos = dragStartPos + dragVector;
+        transform.position = newPos;
+        Vector2 launchDir = dragStartPos - newPos;
+
+        float angle = Mathf.Atan2(launchDir.y, launchDir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    void OnMouseUp()
+    {
+        if (!isDragging) return;
+
+        isDragging = false;
+        rb.bodyType = RigidbodyType2D.Dynamic;
+
+
+        Vector2 launchDir = dragStartPos - (Vector2)transform.position;
+
+        rb.AddForce(launchDir * power, ForceMode2D.Impulse);
+    }
+}
