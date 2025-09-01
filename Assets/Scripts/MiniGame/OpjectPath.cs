31c7eab2ed18504929ed716ced3b2c9b414c83c2
sunwoomk <ahnga3@gmail.com>
2025-09-01 17:45:34 +0900

    Spline Test 중

---
+++ Assets/Scripts/MiniGame/OpjectPath.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class OpjectPath : MonoBehaviour
+{
+    public Spline spline;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
